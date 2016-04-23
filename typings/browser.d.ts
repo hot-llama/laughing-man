a48bce178606dbf09e40ab897176eec644a18028
jmedran <jmedran@gmail.com>
2016-04-23 16:27:28 -0700

    adding express, gulp, and typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/express/index.d.ts" />
+/// <reference path="browser/ambient/gulp/index.d.ts" />
 /// <reference path="browser/ambient/jasmine/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/express/index.d.ts" />
+/// <reference path="main/ambient/gulp/index.d.ts" />
 /// <reference path="main/ambient/jasmine/index.d.ts" />
